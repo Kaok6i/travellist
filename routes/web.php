14b32a372c84e6d3994cdf0d247373f6f27435d8
Kaok6i <ilvivets@bk.ru>
2023-06-20 15:42:36 +0500

    fixed small issue with basket route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/', function () {
+Route::get('/basket', function () {
     return view('basket');
 });
 Route::get('/catalog', function () {
